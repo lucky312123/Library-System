6c8dbd18fbd04376fae3d931359bbbfdb874da9b
lucky321123 <luq951@wp.pl>
2018-04-25 11:56:56 +0200

    add book search

--- src/library/system/CzytelnikOknoController.java
+++ src/library/system/CzytelnikOknoController.java
@@ -1,24 +1,30 @@
-
 package library.system;
 
+import Database.Client;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.application.Application;
 import static javafx.application.Application.STYLESHEET_CASPIAN;
 import static javafx.application.Application.STYLESHEET_MODENA;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import library.system.dialogs.DialogsUtils;
-
 
 public class CzytelnikOknoController implements Initializable {
 
@@ -27,19 +33,23 @@ public class CzytelnikOknoController implements Initializable {
     @FXML
     private TextField tytulSzukanie;
     @FXML
-    private TextField autorSzukanie;
-    @FXML
     private TextField gatunekSzukanie;
+    @FXML
+    private TextField imieASzukanie;
+    @FXML
+    private TextField nazwiskoASzukanie;
     @FXML
     private Button wyszukajKsiazkeBTN;
     @FXML
-    private TableView<?> tableWyszukajKsiazki;
+    private TableView<Ksiazki> tableWyszukajKsiazki;
     @FXML
     private TableColumn<?, ?> columnTytulWyszukaj;
     @FXML
     private TableColumn<?, ?> columnISBNWyszukaj;
     @FXML
-    private TableColumn<?, ?> columnAutorWyszukaj;
+    private TableColumn<?, ?> columnImieWyszukaj;
+    @FXML
+    private TableColumn<?, ?> columnNazwiskoWyszukaj;
     @FXML
     private TableColumn<?, ?> columnData_wydWyszukaj;
     @FXML
@@ -63,19 +73,104 @@ public class CzytelnikOknoController implements Initializable {
     @FXML
     private TableColumn<?, ?> columnData_zwrMoje_ks;
     @FXML
-    private TextField karaPole;
+    private Label karaPole;
     @FXML
-    private TextField ilosc_wypPole;
+    private Label ilosc_wypPole;
+    ObservableList<Ksiazki> ksiazki_list = FXCollections.observableArrayList();
+    @FXML
+    private Button btnWczytajKsiazki;
+    ResultSet rs;
+    PreparedStatement st;
 
-    
+    @FXML
+    private void wczytajKsiazki(ActionEvent event) throws Exception {
+        try {
+            Client client = new Client();
+            client.openConnect();
+            String sql = "SELECT k.tytul,k.ISBN,a.imie_a,a.nazwisko_a,k.data_wyd,g.nazwa_g,s.nazwa_s from ksiazka k, gatunki g, autorzy a, autorzy_ksiazki ak, statusy s where k.id_gatunku=g.id_gatunku and k.id_ksiazki=ak.id_aut_ks and a.id_autora=ak.id_autora and k.status=s.status";
+
+            st = client.connection.prepareStatement(sql);
+            rs = st.executeQuery();
+
+            ksiazki_list.clear();
+            while (rs.next()) {
+                ksiazki_list.add(new Ksiazki(rs.getString("tytul"), rs.getString("ISBN"), rs.getString("imie_a"), rs.getString("nazwisko_a"), rs.getString("data_wyd"), rs.getString("nazwa_g"), rs.getString("nazwa_s")));
+                //System.out.println(rs.getString("tytul") + " " + rs.getString("ISBN")+ " " + rs.getString("nazwisko_a")+ " " + rs.getString("data_wyd")+ " " + rs.getString("nazwa_g")+ " " + rs.getString("nazwa_s"));
+
+            }
+            rs.close();
+            client.connection.close();
+
+        } catch (SQLException sql) {
+            //złe sql
+            System.out.println("bec2" + sql);
+        }
+    }
+
+    @FXML
+    private void wyszukajKsiazki(ActionEvent event) {
+        try {
+            Client client = new Client();
+            client.openConnect();
+            String tytul = tytulSzukanie.getText().trim();
+            String imie_a = imieASzukanie.getText().trim();
+            String nazwisko_a = nazwiskoASzukanie.getText().trim();
+            String gatunek = gatunekSzukanie.getText().trim();
+            String sql2 = "SELECT k.tytul,k.ISBN,a.imie_a,a.nazwisko_a,k.data_wyd,g.nazwa_g,s.nazwa_s from ksiazka k, gatunki g, autorzy a, autorzy_ksiazki ak, statusy s where k.id_gatunku=g.id_gatunku and k.id_ksiazki=ak.id_aut_ks and a.id_autora=ak.id_autora and k.status=s.status and k.tytul=? and a.imie_a=? and a.nazwisko_a=? and g.nazwa_g=?";
+
+            st = client.connection.prepareStatement(sql2);
+
+            st.setString(1, tytul);
+            st.setString(2, imie_a);
+            st.setString(3, nazwisko_a);
+            st.setString(4, gatunek);
+
+            rs = st.executeQuery();
+
+            ksiazki_list.clear();
+            while (rs.next()) {
+                ksiazki_list.add(new Ksiazki(rs.getString("tytul"), rs.getString("ISBN"), rs.getString("imie_a"), rs.getString("nazwisko_a"), rs.getString("data_wyd"), rs.getString("nazwa_g"), rs.getString("nazwa_s")));
+
+            }
+            rs.close();
+            client.connection.close();
+
+        } catch (SQLException sql) {
+            //złe sql
+            System.out.println("bec2" + sql);
+        }
+    }
+
+    @FXML
+    private void wyczyscWyszukiwanie(ActionEvent event) {
+        tytulSzukanie.clear();
+        imieASzukanie.clear();
+        nazwiskoASzukanie.clear();
+        gatunekSzukanie.clear();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
-    }    
+
+        ///Dodanie do tabel elementów list
+        tableWyszukajKsiazki.setItems(null);
+        tableWyszukajKsiazki.setItems(ksiazki_list);
+
+        //Przypisanie komórek z TableView do zmienych
+        //Wyszukanie ksiazek
+        columnTytulWyszukaj.setCellValueFactory(new PropertyValueFactory<>("tytul"));
+        columnISBNWyszukaj.setCellValueFactory(new PropertyValueFactory<>("ISBN"));
+        columnImieWyszukaj.setCellValueFactory(new PropertyValueFactory<>("imie_a"));
+        columnNazwiskoWyszukaj.setCellValueFactory(new PropertyValueFactory<>("nazwisko_a"));
+        columnData_wydWyszukaj.setCellValueFactory(new PropertyValueFactory<>("data_wyd"));
+        columnGatunekWyszukaj.setCellValueFactory(new PropertyValueFactory<>("nazwa_g"));
+        columnStatusWyszukaj.setCellValueFactory(new PropertyValueFactory<>("nazwa_s"));
+
+    }
 
     @FXML
     private void zamknijAplikacje(ActionEvent event) {
-         Optional<ButtonType> result = DialogsUtils.confirmationDialog();
+        Optional<ButtonType> result = DialogsUtils.confirmationDialog();
         if (result.get() == ButtonType.OK) {
             Platform.exit();
         }
@@ -95,5 +190,5 @@ public class CzytelnikOknoController implements Initializable {
     private void aboutApplication(ActionEvent event) {
         DialogsUtils.dialogAboutAplication();
     }
-    
+
 }
---
+++ src/library/system/Ksiazki.java
@@ -0,0 +1,82 @@
+package library.system;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Ksiazki {
+
+    private final SimpleStringProperty tytul;
+    private final SimpleStringProperty ISBN;
+    private final SimpleStringProperty imie_a;
+    private final SimpleStringProperty nazwisko_a;
+    private final SimpleStringProperty data_wyd;
+    private final SimpleStringProperty nazwa_g;
+    private final SimpleStringProperty nazwa_s;
+
+    public Ksiazki(String tytul, String ISBN, String imie_a, String nazwisko_a, String data_wyd, String nazwa_g, String nazwa_s) {
+        this.tytul = new SimpleStringProperty(tytul);
+        this.ISBN = new SimpleStringProperty(ISBN);
+        this.imie_a = new SimpleStringProperty(imie_a);
+        this.nazwisko_a = new SimpleStringProperty(nazwisko_a);
+        this.data_wyd = new SimpleStringProperty(data_wyd);
+        this.nazwa_g = new SimpleStringProperty(nazwa_g);
+        this.nazwa_s = new SimpleStringProperty(nazwa_s);
+
+    }
+
+    public String getTytul() {
+        return tytul.get();
+    }
+
+    public void setTytul(String value) {
+        tytul.set(value);
+    }
+
+    public String getISBN() {
+        return ISBN.get();
+    }
+
+    public void setISNB(String value) {
+        ISBN.set(value);
+    }
+
+    public String getData_wyd() {
+        return data_wyd.get();
+    }
+
+    public void setData_wyd(String value) {
+        data_wyd.set(value);
+    }
+
+    public String getImie_a() {
+        return imie_a.get();
+    }
+
+    public void setImie_a(String value) {
+        imie_a.set(value);
+    }
+
+    public String getNazwisko_a() {
+        return nazwisko_a.get();
+    }
+
+    public void setNazwisko_a(String value) {
+        nazwisko_a.set(value);
+    }
+
+    public String getNazwa_g() {
+        return nazwa_g.get();
+    }
+
+    public void setNazwa_g(String value) {
+        nazwa_g.set(value);
+    }
+
+    public String getNazwa_s() {
+        return nazwa_s.get();
+    }
+
+    public void setNazwa_s(String value) {
+        nazwa_s.set(value);
+    }
+
+}
